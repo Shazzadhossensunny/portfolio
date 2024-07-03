51d80d3e234ec8aa4fbbe9ed3d6e927e162d4b44
Shazzadhossensunny <shazzadhossensunny@gmail.com>
2024-07-03 16:41:44 +0600

    portfoilo website create

---
+++ index.js
@@ -0,0 +1,60 @@
+// ---
+const hamMenuBtn = document.querySelector(".header__main-ham-menu-cont");
+const smallMenu = document.querySelector(".header__sm-menu");
+const headerHamMenuBtn = document.querySelector(".header__main-ham-menu");
+const headerHamMenuCloseBtn = document.querySelector(
+  ".header__main-ham-menu-close"
+);
+const headerSmallMenuLinks = document.querySelectorAll(".header__sm-menu-link");
+
+hamMenuBtn.addEventListener("click", () => {
+  if (smallMenu.classList.contains("header__sm-menu--active")) {
+    smallMenu.classList.remove("header__sm-menu--active");
+  } else {
+    smallMenu.classList.add("header__sm-menu--active");
+  }
+  if (headerHamMenuBtn.classList.contains("d-none")) {
+    headerHamMenuBtn.classList.remove("d-none");
+    headerHamMenuCloseBtn.classList.add("d-none");
+  } else {
+    headerHamMenuBtn.classList.add("d-none");
+    headerHamMenuCloseBtn.classList.remove("d-none");
+  }
+});
+
+for (let i = 0; i < headerSmallMenuLinks.length; i++) {
+  headerSmallMenuLinks[i].addEventListener("click", () => {
+    smallMenu.classList.remove("header__sm-menu--active");
+    headerHamMenuBtn.classList.remove("d-none");
+    headerHamMenuCloseBtn.classList.add("d-none");
+  });
+}
+
+// ---
+const headerLogoConatiner = document.querySelector(".header__logo-container");
+
+headerLogoConatiner.addEventListener("click", () => {
+  location.href = "/";
+});
+
+// send mail
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+  const serviceID = "service_r0ffn07";
+  const templateID = "template_2lx38fm";
+
+  emailjs
+    .send(serviceID, templateID, params)
+    .then((res) => {
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
+      console.log(res);
+      alert("Your message sent successfully");
+    })
+    .catch((err) => console.log(err));
+}
